b588a429389bc4b52804dbae07fe2b253a679e8a
Santiago Quintero <86493587+santiqc@users.noreply.github.com>
2025-02-18 10:25:00 -0500

    Creacion proyect

---
+++ src/main/java/com/procesos/colas/ColasApplication.java
@@ -0,0 +1,13 @@
+package com.procesos.colas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/procesos/colas/ColasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.procesos.colas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
